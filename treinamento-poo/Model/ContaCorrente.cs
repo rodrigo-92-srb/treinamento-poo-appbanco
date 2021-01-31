a1f0b23ffa6f9cd3c88b80536d7a74f5493893c9
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2021-01-31 13:36:02 -0300

    Criação da Classe ContaCorrente.cs

--- treinamento-poo/Model/ContaCorrente.cs
+++ treinamento-poo/Model/ContaCorrente.cs
@@ -2,6 +2,9 @@
 {
     public class ContaCorrente 
     {
-       
+        public string Titular { get; set; }
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+        public double Saldo { get; set; }
     }
 }
